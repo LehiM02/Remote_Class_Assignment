98b157034df926ae5474a9dcda1c46bbacc50e4e
LehiM <d00519401@utahtech.edu>
2026-01-22 08:06:44 -0700

    added timers to text

--- age_guesser.py/age_guesser.py
+++ age_guesser.py/age_guesser.py
@@ -1,10 +1,14 @@
 import random
+from time import sleep
 
 def main():
     print("Greetings user. I will attempt" \
     " to guess your age.")
+    sleep(3)
     name = input('Please enter your name:')
+    sleep(3)
     print("let's begin....")
+    sleep(3)
     while True:
         guess = str(random.randrange(15, 30))
         response = input('Are you ' + guess + ' years old? ')
